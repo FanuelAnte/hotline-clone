8a0b2f4d88851accd0d876e9283535a1bcbd4123
Fanuel Anteneh <apexfanuel@gmail.com>
2025-06-06 13:34:00 +0300

    cards container

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,17 @@ function App() {
       <div className="w-screen h-screen bg-black flex items-center justify-center">
         <div className="w-full h-full md:w-[960px] md:h-[540px] p-1 md:p-3">
           <div className="flex flex-col w-full h-full border-2 rounded-xl md:border-golden-yellow bg-gradient-to-r from-light-blue-edge via-light-blue-center to-light-blue-edge">
-            <div></div>
+            <div className="flex w-full h-full items-center p-2">
+              <div className="flex flex-col items-center border-1 border-blue-200/35 justify-between bg-dark-blue w-full h-[188px] md:h-1/2 rounded-xl inset-shadow-2xs">
+                <img src="src/assets/hotline-arrow.svg" alt="arrow1" />
+                <img src="src/assets/cards.png" alt="Cards" />
+                <img
+                  src="src/assets/hotline-arrow.svg"
+                  alt="arrow2"
+                  className="rotate-180"
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>
